b75dabf3ef74d4cc0792c86c6cb03572081f87a9
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2024-06-20 12:08:09 +0100

    fixing

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
 const express = require("express");
-const app = express();
 const bodyParser = require('body-parser'); 
-const port = 3000;
-const path = require("path");
+const app = express();
+const port = 5000;
 
 app.set("view engine", "ejs");
 
-// Serve static files from the 'static' directory
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     // Render index.ejs from the 'views' directory
-    res.render("index.ejs");
-    
-    
+    res.render("index");
 });
 
 app.get("/blog", (req, res) => {
     // Get the value of the clicked button
     const category = req.query.category;
     console.log(`Category: ${category}`);
-    // Render the blog page or perform other actions
-    res.render("blog.ejs");
-   
     
+    // Render the blog page or perform other actions
+    res.render("blog", { category });
 });
 
-app.get("/submit", (req, res)=>{
-    const email = req.query.email;
-    const name = req.query.name;
-    const text = req.query.text;
-    console.log(email);
-    console.log(name);
-    res.send("Thanks");
-        
-})
+app.post("/submit", (req, res) => {
+    const { email, name, text } = req.body;
+    const data = { name, email, text };
+    console.log(data);
+    
+    res.send(`<html> 
+            <head> 
+                <body>
+                <h3>Thanks</h3> 
+                    <script> setTimeout(() => { window.location.href = "/"; }, 2000);</script>
+                </body>
+            </head>
+         </html>`);
+
+    
+});
 
 app.get("/about", (req, res) => {
-    res.render("about.ejs")
-   
-    
+    res.render("about");
 });
-
 
 app.listen(port, () => {
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on port ${port}`);
 });
+
